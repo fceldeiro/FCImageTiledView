b954797ddc7d235866f8e00808756b6f28d0c20c
Fabián Celdeiro <fabian.celdeiro@mercadolibre.com>
2015-02-06 00:08:15 -0300

    improvement in content size

--- FCImageTiledView/FCImageTiledView/MyCustomView.m
+++ FCImageTiledView/FCImageTiledView/MyCustomView.m
@@ -42,7 +42,7 @@
                             [mainBundle pathForResource:@"icoTc_bank_transfer" ofType:@"png"]];
 
     
-    for (int i = 0 ;i<20; i++){
+    for (int i = 0 ;i<120; i++){
         
         NSString* filenameToUse = nil;
         
@@ -95,17 +95,22 @@
 
 -(void) layoutMultineInstrinsicHeight{
     
-    static const CGFloat spacing = 5.0f;
+    static const CGFloat internalSpacingX = 5.0f;
+    static const CGFloat internalSpacingY = 5.0f;
     
-    CGFloat maxHeightForRow = spacing;
-    CGFloat currentYPosition = spacing;
+    CGFloat maxHeightForRow = 0;
+    CGFloat currentYPosition = 0;
     
     UIView * previousImageView = nil;
     for ( int i = 0 ; i<self.subviews.count ; i++){
         
         UIView * currentImage = self.subviews[i];
         
-        CGPoint nextPosition = CGPointMake(previousImageView.frame.origin.x + spacing + previousImageView.frame.size.width, currentYPosition);
+        CGFloat currentSpacingX = 0;
+        if (i > 0){
+            currentSpacingX = internalSpacingX;
+        }
+        CGPoint nextPosition = CGPointMake(previousImageView.frame.origin.x + currentSpacingX + previousImageView.frame.size.width, currentYPosition);
         
         if (currentImage.frame.size.height  > maxHeightForRow){
             maxHeightForRow = currentImage.frame.size.height;
@@ -113,11 +118,11 @@
         }
         
         //Me pase
-        if (nextPosition.x + currentImage.frame.size.width > self.frame.size.width + spacing ){
+        if (nextPosition.x + currentImage.frame.size.width > self.frame.size.width ){
 
             maxHeightForRow = currentImage.frame.size.height;
-            currentYPosition = previousImageView.frame.origin.y + maxHeightForRow + spacing;
-            nextPosition = CGPointMake(spacing, currentYPosition);
+            currentYPosition = previousImageView.frame.origin.y + maxHeightForRow + internalSpacingY;
+            nextPosition = CGPointMake(0, currentYPosition);
         }
        
         [currentImage setFrame:CGRectMake(nextPosition.x, nextPosition.y, currentImage.frame.size.width, currentImage.frame.size.height)];
@@ -142,11 +147,18 @@
 
 - (CGSize) intrinsicContentSizeMultipleLines{
     
-    UIView * lastObject = [self.subviews lastObject];
-    // CGFloat width =  lastObject.frame.origin.x + lastObject.frame.size.width + 5;
-    CGFloat height = lastObject.frame.origin.y + lastObject.frame.size.height + 5;
-    
-    return CGSizeMake(UIViewNoIntrinsicMetric, height);
+    CGFloat maxX = 0;
+    CGFloat maxY = 0;
+    for (UIView * view in self.subviews){
+        if (view.frame.origin.x + view.frame.size.width > maxX){
+            maxX = view.frame.origin.x + view.frame.size.width;
+        }
+        if (view.frame.origin.y + view.frame.size.height > maxY){
+            maxY = view.frame.origin.y + view.frame.size.height;
+        }
+
+    }
+    return CGSizeMake(maxX, maxY);
 
 }
 
